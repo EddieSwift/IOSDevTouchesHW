4c4f1889674c2aee6e915a8493e024d9ddf63099
EddieSwift <37484195+EddieSwift@users.noreply.github.com>
2018-11-04 21:59:51 +0200

    Initial Commit

---
+++ IOSDevTouchesHW/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IOSDevTouchesHW
+//
+//  Created by Eduard Galchenko on 11/4/18.
+//  Copyright © 2018 Eduard Galchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IOSDevTouchesHW/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IOSDevTouchesHW
+//
+//  Created by Eduard Galchenko on 11/4/18.
+//  Copyright © 2018 Eduard Galchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
